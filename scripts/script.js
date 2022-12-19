2bf92b078edf1be186f49de626066e6246e2f458
Rob Carruthers <rob-carruthers@users.noreply.github.com>
2022-12-19 12:16:59 +0000

    Implement decimals and floating-point operations

--- scripts/script.js
+++ scripts/script.js
@@ -24,12 +24,14 @@ let calculatorDisplayValue = 0;
 let firstOperand = 0;
 let currentOperator = "";
 const precision = 14;
+let calculatorDisplayDecimals = 0;
 const calculatorDisplay = document.querySelector(".calculatorDisplay")
 
 function reset() {
     calculatorDisplayValue = 0;
     firstOperand = 0;
     currentOperator = "";
+    calculatorDisplayDecimals = 0;
     calculatorDisplay.textContent = calculatorDisplayValue;
 }
 
@@ -43,7 +45,21 @@ document.querySelector("#buttonClear").addEventListener(
 
 // Function for updating calculatorDisplayValue and the display itself
 function sendNumeralToDisplay(n) {
-    let newValue = (calculatorDisplayValue * 10) + n;
+    let newValue = 0;
+    
+    // If a decimal number, handle this gracefully
+    if (calculatorDisplayDecimals > 0) {
+        // First update the value so that typing more numbers adds decimals on the end
+        newValue = calculatorDisplayValue + (n / Math.pow(10, calculatorDisplayDecimals))
+        // Use rounding to avoid floating-point rounding errors cluttering the display
+        newValue = Math.round(newValue * Math.pow(10, calculatorDisplayDecimals)) / Math.pow(10, calculatorDisplayDecimals)
+        // Store our floating-point position
+        calculatorDisplayDecimals++;
+    }
+    else {
+        // If no decimals, add a digit on the end as normal.
+        newValue = (calculatorDisplayValue * 10) + n;
+    }
     calculatorDisplayValue = newValue;
     calculatorDisplay.textContent = calculatorDisplayValue;
 }
@@ -64,6 +80,7 @@ for (const operator of ["Add", "Subtract", "Multiply", "Divide"] ) {
             // Check if this is the first operation or one of a chain
             if (currentOperator === "" ) {
                 firstOperand = calculatorDisplayValue;
+                calculatorDisplayDecimals = 0;
                 calculatorDisplayValue = 0;
             }
             // If part of a chain, calculate the first result and then
@@ -74,6 +91,7 @@ for (const operator of ["Add", "Subtract", "Multiply", "Divide"] ) {
                 firstOperand = result;
                 calculatorDisplay.textContent = Math.round(result * Math.pow(10, precision)) / Math.pow(10, precision);
                 // reset the display value for when a new number is typed.
+                calculatorDisplayDecimals = 0;
                 calculatorDisplayValue = 0;
             }
             // Set the current operator from the content of
@@ -93,3 +111,11 @@ document.querySelector("#buttonEquals").addEventListener(
     }
 )
 
+document.querySelector("#buttonDecimal").addEventListener(
+    'click', () => {
+        // If no decimal places at this point, add one
+        if (calculatorDisplayDecimals < 1) {
+            calculatorDisplayDecimals = 1;
+        }
+    }
+);
