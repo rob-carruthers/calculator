f8e826ce54f1fc222707ab06384d64d40fb6110a
Rob Carruthers <rob-carruthers@users.noreply.github.com>
2022-12-19 11:28:26 +0000

    Clean up code - slim down operate() section

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,21 @@
-// Arithmetic functions with two operators
-
-function add(a, b) {
-    return a + b;
-};
-
-function subtract(a, b) {
-    return a - b;
-};
-
-function multiply(a, b) {
-    return a * b;
-};
-
-function divide(a, b) {
-    return a / b;
-}
+// Operate function for all arithmetic operations
 
 function operate(operator, a, b) {
     switch(operator) {
         case "+":
-            return add(a, b);
+            return a + b;
             break;
         case "-":
-            return subtract(a, b);
+            return a - b;
             break;
         case "*":
-            return multiply(a, b);
+            return a * b;
             break;
         case "/":
-            return divide(a, b);
+            return a / b;
             break;
         default:
-            return "Operator not recognised."
+            return "Error"
     }
 }
 
