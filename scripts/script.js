954187e53558dc52682840008ace7098ac47dcde
Rob Carruthers <rob-carruthers@users.noreply.github.com>
2022-12-19 09:49:22 +0000

    Create basic arithmetic backend functions

---
+++ scripts/script.js
@@ -0,0 +1,37 @@
+// arithmetic functions with two operators
+
+function add(a, b) {
+    return a + b;
+};
+
+function subtract(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a * b;
+};
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(operator, a, b) {
+    switch(operator) {
+        case "+":
+            return add(a, b);
+            break;
+        case "-":
+            return subtract(a, b);
+            break;
+        case "*":
+            return multiply(a, b);
+            break;
+        case "/":
+            return divide(a, b);
+            break;
+        default:
+            return "Operator not recognised."
+    }
+}
+
