78a2f419c5ee760e5d494d7f6e61a73d24ef095d
Rob Carruthers <rob-carruthers@users.noreply.github.com>
2022-12-19 10:39:41 +0000

    Create EventListeners for numeric buttons

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-// arithmetic functions with two operators
+// Arithmetic functions with two operators
 
 function add(a, b) {
     return a + b;
@@ -35,3 +35,25 @@ function operate(operator, a, b) {
     }
 }
 
+
+
+// Initial setup
+const calculatorDisplay = document.querySelector(".calculatorDisplay")
+let calculatorDisplayValue = 0;
+calculatorDisplay.textContent = calculatorDisplayValue;
+
+// Function for updating calculatorDisplayValue and the display itself
+function sendNumeralToDisplay(n) {
+    let newValue = (calculatorDisplayValue * 10) + n;
+    calculatorDisplayValue = newValue;
+    calculatorDisplay.textContent = calculatorDisplayValue;
+}
+
+// Set up listeners for numeric keys
+for (let i = 0; i < 10; i++) {
+    document.querySelector("#button" + i).addEventListener(
+        'click', (e) => {
+            sendNumeralToDisplay(parseInt(e.target.textContent)
+        )
+    });
+}
