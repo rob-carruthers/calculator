78a965280b5ab616e3c1dcbfd437856e1cd376bc
Rob Carruthers <12486527+rob-carruthers@users.noreply.github.com>
2022-12-24 10:44:11 +0000

    Implement operands display above result

--- scripts/script.js
+++ scripts/script.js
@@ -20,46 +20,48 @@ function operate(operator, a, b) {
 }
 
 function pressOperatorKey(e) {
-    debugger;
     backspaceEnabled = false;
     // Check if this is the first operation or one of a chain
     if (currentOperator === "" ) {
-        firstOperand = calculatorDisplayValue;
-        calculatorDisplayDecimals = 0;
-        calculatorDisplayValue = 0;
+        firstOperand = outputDisplayValue;
+        outputDisplayDecimals = 0;
+        outputDisplayValue = 0;
     }
     // If part of a chain, calculate the first result and then
     // make this the result the first operand of the next
     // operation.
     else {
-        const result = operate(currentOperator, firstOperand, calculatorDisplayValue);
+        const result = operate(currentOperator, firstOperand, outputDisplayValue);
         firstOperand = result;
-        calculatorDisplay.textContent = Math.round(result * Math.pow(10, precision)) / Math.pow(10, precision);
+        outputDisplay.textContent = Math.round(result * Math.pow(10, precision)) / Math.pow(10, precision);
         // reset the display value for when a new number is typed.
-        calculatorDisplayDecimals = 0;
-        calculatorDisplayValue = 0;
+        outputDisplayDecimals = 0;
+        outputDisplayValue = 0;
     }
     // Set the current operator from the content of
     // the button which has been clicked
     currentOperator = e.target.textContent;
+    operandsDisplay.textContent = firstOperand.toString() + " " + currentOperator;
 
 };
 
 // Initial setup
-let calculatorDisplayValue = 0;
+let outputDisplayValue = 0;
 let firstOperand = 0;
 let currentOperator = "";
 let backspaceEnabled = false;
 const precision = 14;
-let calculatorDisplayDecimals = 0;
-const calculatorDisplay = document.querySelector(".calculatorDisplay")
+let outputDisplayDecimals = 0;
+const outputDisplay = document.querySelector("#outputDisplay");
+const operandsDisplay = document.querySelector("#operandsDisplay");
 
 function reset() {
-    calculatorDisplayValue = 0;
+    outputDisplayValue = 0;
     firstOperand = 0;
     currentOperator = "";
-    calculatorDisplayDecimals = 0;
-    calculatorDisplay.textContent = calculatorDisplayValue;
+    outputDisplayDecimals = 0;
+    outputDisplay.textContent = outputDisplayValue;
+    operandsDisplay.textContent = "";
 }
 
 reset()
@@ -70,26 +72,26 @@ document.querySelector("#buttonClear").addEventListener(
     () => reset()
 );
 
-// Function for updating calculatorDisplayValue and the display itself
+// Function for updating outputDisplayValue and the display itself
 function sendNumeralToDisplay(n) {
     let newValue = 0;
 
     // If a decimal number, handle this gracefully
-    if (calculatorDisplayDecimals > 0) {
+    if (outputDisplayDecimals > 0) {
         // First update the value so that typing more numbers adds decimals on the end
-        newValue = calculatorDisplayValue + (n / Math.pow(10, calculatorDisplayDecimals))
+        newValue = outputDisplayValue + (n / Math.pow(10, outputDisplayDecimals))
         // Use rounding to avoid floating-point rounding errors cluttering the display
-        newValue = Math.round(newValue * Math.pow(10, calculatorDisplayDecimals)) / Math.pow(10, calculatorDisplayDecimals)
+        newValue = Math.round(newValue * Math.pow(10, outputDisplayDecimals)) / Math.pow(10, outputDisplayDecimals)
         // Store our floating-point position
-        calculatorDisplayDecimals++;
+        outputDisplayDecimals++;
     }
     else {
         // If no decimals, add a digit on the end as normal.
-        newValue = (calculatorDisplayValue * 10) + n;
+        newValue = (outputDisplayValue * 10) + n;
     }
     backspaceEnabled = true;
-    calculatorDisplayValue = newValue;
-    calculatorDisplay.textContent = calculatorDisplayValue;
+    outputDisplayValue = newValue;
+    outputDisplay.textContent = outputDisplayValue;
 }
 
 // Set up listeners for numeric keys
@@ -110,11 +112,15 @@ for (const operator of ["Add", "Subtract", "Multiply", "Divide"] ) {
 document.querySelector("#buttonEquals").addEventListener(
     'click', () => {
         if (!(currentOperator === "")) {
-            const result = operate(currentOperator, firstOperand, calculatorDisplayValue);
-            calculatorDisplayValue = result;
-            calculatorDisplay.textContent = Math.round(result * Math.pow(10, precision)) / Math.pow(10, precision);
+            const result = operate(currentOperator, firstOperand, outputDisplayValue);
+            operandsDisplay.textContent = firstOperand.toString() + " " + currentOperator + " " + outputDisplayValue.toString() + " = ";
+            outputDisplayValue = result;
+            outputDisplay.textContent = Math.round(result * Math.pow(10, precision)) / Math.pow(10, precision);
+            
+
             currentOperator = "";
             backspaceEnabled = false;
+        
         }
     }
 )
@@ -122,9 +128,9 @@ document.querySelector("#buttonEquals").addEventListener(
 document.querySelector("#buttonDecimal").addEventListener(
     'click', () => {
         // If no decimal places at this point, add one
-        if (calculatorDisplayDecimals < 1) {
-            calculatorDisplayDecimals = 1;
-            calculatorDisplay.textContent += "."
+        if (outputDisplayDecimals < 1) {
+            outputDisplayDecimals = 1;
+            outputDisplay.textContent += "."
         }
     }
 );
@@ -133,17 +139,17 @@ document.querySelector("#buttonBackspace").addEventListener(
     'click', () => {
         // Remove last digit of display through string manipulation
         // (A little dirty)
-        if (!(calculatorDisplay === 0) && backspaceEnabled) {
-            let numString = calculatorDisplay.textContent;
+        if (!(outputDisplay === 0) && backspaceEnabled) {
+            let numString = outputDisplay.textContent;
 
             numString = numString.slice(0, -1);
 
-            if (calculatorDisplayDecimals > 0) {
-                calculatorDisplayDecimals -= 1;
+            if (outputDisplayDecimals > 0) {
+                outputDisplayDecimals -= 1;
             };
 
-            calculatorDisplayValue = Number(numString);
-            calculatorDisplay.textContent = numString;
+            outputDisplayValue = Number(numString);
+            outputDisplay.textContent = numString;
         }
     }
 );
